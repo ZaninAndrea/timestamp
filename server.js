f4cd306f6010f34432e4c4554f30fe5e7c0f50d9
Zanin Andrea <zanin_andrea@outlook.com>
2016-12-27 14:24:57 +0100

    🦃🐊 Updated with Gomix

--- server.js
+++ server.js
@@ -4,36 +4,28 @@
 // init project
 var express = require('express');
 var app = express();
+app.get('/*', function (req, res) {
+  var response={error:"error"};
+  var d;
+  var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+  try {
+    d = new Date(Date.parse(req.url.replace(/%20/g," ").substring(1)));
+  }  catch(err) {  }
+  try {
+    if (parseInt(req.url.substring(1))){
+      var time=parseInt(req.url.substring(1));
+      d = new Date(time*1000);
+    }
+  }  catch(err) {  }
+  
+  if (d!==null && d.toString!=="Invalid Date"){
+    response={"natural":months[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear(),"unix":+ d};
+  }else{
+    response={"unix":null,"natural":null};
+  }
+  res.send(JSON.stringify(response));
+})
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
